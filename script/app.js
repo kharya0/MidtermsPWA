1ed879db03dd0eb306ebed0fe77346b206eab8a6
kharya0 <kkdvillegas@gmail.com>
2018-04-06 23:39:23 +0800

    removed some files

--- script/app.js
+++ script/app.js
@@ -1,12 +1,27 @@
-function validate() {
-    var username = document.getElementsByName("uname").value;
-    var password = document.getElementsByName("psw").value;
-    
-    if (username == "inst" && password == "1234") {
-        window.location = "../html/newsched.html";
-        return false;
-    }
-    
+var sched1 = "6:00 - 7:00";
+var sched2 = "7:00 - 8:00";
+var locSkating = "Near the Skating Rink";
+var locPark = "Near the Children's Park";
+var locSolibao = "Near Solibao";
+var locRose = "Near the Rose Garden";
+var time1 = new Date("April 6, 2018 6:00:00");
+var time2 = new Date("April 6, 2018 7:00:00");
+
+function Instructor(name, schedule, location, time) {
+    this.name = name;
+    this.schedule = schedule;
+    this.location = location;
+    this.time = time;
 }
 
-validate();
+var instMel = new Instructor("Mel", sched1, locSkating, time1.getHours());
+var instXylon = new Instructor("Xylon", sched1, locSkating, time1.getHours());
+var instJules = new Instructor("Jules", sched1, locSkating, time1.getHours());
+var instFrank = new Instructor("Frank", sched1, locPark, time1.getHours());
+var instJr = new Instructor("JR", sched1, locPark, time1.getHours());
+var instKevin = new Instructor("Kevin", sched1, locSolibao, time1.getHours());
+var instSarah = new Instructor("Sarah", sched1, locSolibao, time1.getHours());
+var instArman = new Instructor("Arman", sched2, locRose, time2.getHours());
+
+
+
