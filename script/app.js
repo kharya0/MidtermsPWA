f3ad645272fc8b3b78526f9badc5e8e94d9816c8
kharya0 <kkdvillegas@gmail.com>
2018-04-10 22:58:46 +0800

    changed time variables

--- script/app.js
+++ script/app.js
@@ -6,13 +6,10 @@ var locSolibao = "Near Solibao";
 var locRose = "Near the Rose Garden";
 var date1 = new Date("April 6, 2018 6:00:00");
 var date2 = new Date("April 6, 2018 7:00:00");
-var hour1 = date1.getHours();
-var hour2 = date2.getHours();
-var min1 = date1.getMinutes();
-var min2 = date2.getMinutes();
+var time1 = date1.getTime();
+var time2 = date2.getTime();
 var currentDate = new Date();
-var currentHours = currentDate.getHours();
-var currentMin = currentDate.getMinutes();
+var currentTime = currentDate.getTime();
 
 function Instructor(name, schedule, location, hour, min) {
     this.name = name;
@@ -22,18 +19,18 @@ function Instructor(name, schedule, location, hour, min) {
     this.min = min;
 }
 
-var instMel = new Instructor("Mel", sched1, locSkating, hour1, min1);
-var instXylon = new Instructor("Xylon", sched1, locSkating, hour1, min1);
-var instJules = new Instructor("Jules", sched1, locSkating, hour1, min1);
-var instFrank = new Instructor("Frank", sched1, locPark, hour1, min1);
-var instJr = new Instructor("JR", sched1, locPark, hour1, min1);
-var instKevin = new Instructor("Kevin", sched1, locSolibao, hour1, min1);
-var instSarah = new Instructor("Sarah", sched1, locSolibao, hour1, min1);
-var instArman = new Instructor("Arman", sched2, locRose, hour2, min2);
-var instGrace = new Instructor("Grace", sched2, locRose, hour2, min2);
-var instMarvin = new Instructor("Marvin", sched2, locRose, hour2, min2);
-var instJuvy = new Instructor("Juvy", sched2, locRose, hour2, min2);
-var instJoseph = new Instructor("Joseph", sched2, locRose, hour2, min2);
+var instMel = new Instructor("Mel", sched1, locSkating, time1);
+var instXylon = new Instructor("Xylon", sched1, locSkating, time1);
+var instJules = new Instructor("Jules", sched1, locSkating, time1);
+var instFrank = new Instructor("Frank", sched1, locPark, time1);
+var instJr = new Instructor("JR", sched1, locPark, time1);
+var instKevin = new Instructor("Kevin", sched1, locSolibao, time1);
+var instSarah = new Instructor("Sarah", sched1, locSolibao, time1);
+var instArman = new Instructor("Arman", sched2, locRose, time2);
+var instGrace = new Instructor("Grace", sched2, locRose, time2);
+var instMarvin = new Instructor("Marvin", sched2, locRose, time2);
+var instJuvy = new Instructor("Juvy", sched2, locRose, time2);
+var instJoseph = new Instructor("Joseph", sched2, locRose, time2);
 
 var instArray = [instMel, instXylon, instJules, instFrank, instJr, instKevin, instSarah, instArman, instGrace, instMarvin, instJuvy, instJoseph];
 
