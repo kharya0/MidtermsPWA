048660bf445502c54ddeea3124a3bd86489e6281
kharya0 <kkdvillegas@gmail.com>
2018-04-11 22:27:36 +0800

    added function inside computeTimeLeft

--- script/app.js
+++ script/app.js
@@ -108,9 +108,19 @@ function computeTimeLeft(instArray){
     var hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((interval % (1000 * 60)) / 1000);
-    console.log(instArray);
     console.log(hours + 'h ' + minutes + 'm ' + seconds + 's ');
     console.log(arrayDate);
+    
+    var countdownArray = [];
+    
+    function timer(name, place, date, time, countdown) {
+        this.name = name;
+        this.place = place;
+        this.date = place;
+        this.time = time;
+        this.countdown = countdown;
+    }
+    
 }
 
 
